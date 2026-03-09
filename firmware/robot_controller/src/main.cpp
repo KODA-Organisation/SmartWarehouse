e49261e1cc3d35fe9a2b9400020d68fc5cd9ed7b
NeoPearl <irofiterps3@gmail.com>
2026-03-09 13:18:14 +0100

    basic motor code

--- firmware/robot_controller/src/main.cpp
+++ firmware/robot_controller/src/main.cpp
@@ -1,11 +1,34 @@
 #include <Arduino.h>
 
-// CLEARED ALL UNNECESSARY CODE
+const int IN1 = 4;
+const int IN2 = 5;
+
+const int IN3 = 6;
+const int IN4 = 7;
 
 void setup() {
+    Serial.begin(115200);
+    
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
 
+    Serial.println("Neimal Engine: Ready for test");
 }
 
 void loop() {
+    Serial.println("FULL FORWARD");
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    delay(3000);
 
+    Serial.println("STOP");
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    delay(2000);
 }
