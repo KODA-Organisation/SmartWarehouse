2ffeb51ed0e4a69b7c14880e0d77d112fbf653c4
NeoPearl <irofiterps3@gmail.com>
2026-03-09 22:23:27 +0100

    USE connecting and testing

--- firmware/robot_controller/src/main.cpp
+++ firmware/robot_controller/src/main.cpp
@@ -1,34 +1,34 @@
 #include <Arduino.h>
 
-const int IN1 = 4;
-const int IN2 = 5;
-
-const int IN3 = 6;
-const int IN4 = 7;
+const int trigPin = 8;
+const int echoPin = 9;
 
 void setup() {
     Serial.begin(115200);
-    
-    pinMode(IN1, OUTPUT);
-    pinMode(IN2, OUTPUT);
-    pinMode(IN3, OUTPUT);
-    pinMode(IN4, OUTPUT);
-
-    Serial.println("Neimal Engine: Ready for test");
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    Serial.println("Радар активирован. Ищем препятствия...");
 }
 
 void loop() {
-    Serial.println("FULL FORWARD");
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    delay(3000);
+    // Очищаем пин перед выстрелом
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
 
-    Serial.println("STOP");
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
-    delay(2000);
+    // Стреляем ультразвуком (импульс 10 микросекунд)
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    // Засекаем, через сколько микросекунд вернулось эхо
+    long duration = pulseIn(echoPin, HIGH);
+
+    // Формула перевода времени в сантиметры (скорость звука)
+    int distance = duration * 0.034 / 2;
+
+    Serial.print("Дистанция: ");
+    Serial.print(distance);
+    Serial.println(" см");
+
+    delay(500);
 }
